fd2419775c57e587bf347489bfba42d6395706d3
Christine Ash <christine.ash7@gmail.com>
2019-04-25 12:40:58 -0400

    Added post function

--- routes/test.js
+++ routes/test.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
 
 module.exports = router;
